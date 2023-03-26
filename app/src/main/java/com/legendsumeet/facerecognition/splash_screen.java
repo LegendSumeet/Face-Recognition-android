61905edc5566cc4dfec76994e265b61db5d12246
LegendSumeet <sumeetgithub@gmail.com>
2023-03-26 21:13:14 +0530

    done

---
+++ app/src/main/java/com/legendsumeet/facerecognition/splash_screen.java
@@ -0,0 +1,25 @@
+package com.legendsumeet.facerecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(splash_screen.this,sponsor.class);
+                finish();
+                startActivity(intent);
+            }
+        },2500);
+    }
+}
---
+++ app/src/main/java/com/legendsumeet/facerecognition/sponsor.java
@@ -0,0 +1,26 @@
+package com.legendsumeet.facerecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class sponsor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sponsor);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(sponsor.this,MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        },2500);
+    }
+}
